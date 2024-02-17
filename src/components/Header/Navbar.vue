9e98deef3372fe92e2fe2f86d4d39343129f9c98
Sharid kayes <sharidkayes@gmail.com>
2024-02-17 22:09:58 +0800

    Update Navbar.vue

--- src/components/Header/Navbar.vue
+++ src/components/Header/Navbar.vue
@@ -17,7 +17,7 @@
             </li>
 
             <li class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-             <RouterLink to="/about"> 关于我们</RouterLink>
+             <RouterLink to="/about">about</RouterLink>
             </li>
             <li>
               <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">新闻资讯</a>
